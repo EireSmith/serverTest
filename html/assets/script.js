c852946bffba8d0ffbc3e16859bc80aa12dca10f
Rob <71661538+EireSmith@users.noreply.github.com>
2022-05-21 17:41:22 +0100

    update, delete button fix. Working!

--- html/assets/script.js
+++ html/assets/script.js
@@ -6,8 +6,37 @@ const clearAllBtn = document.querySelector(".footer clear");
 baseURL = 'https://robert.dbsprojects.ie:8080/'
 
 
+let show = () => {
+    fetch(baseURL).then(
+        response => {
+            response.json().then(
+                data => {
+                    console.log(data);
+                    if(data.Results.length > 0){
+                        var temp = "";
+                        data.Results.forEach((x) => {
+                            
+                            temp += "<td>"+x.ID+"</td>";
+                            temp += "<td>"+x.Name+"</td>";
+                            temp += "<td>"+x.Email+"</td>";
+                            temp += "<td>"+ "<button class='delBtn' onclick='deleteStudent("+x.ID+")'><i class='fa fa-ban'></i> </button>" +
+                            "</td>"
+                            temp += "<td>"+ "<button class='delBtn' onclick='updateStudent("+x.ID+")'><i class='fa fa-pen-to-square'></i> </button>" +
+                            "</td></tr>"
+                            
+                        })
+                    
+                        document.getElementById("tab1").innerHTML = temp;
+                    
+                    }
+                }  
+            )
+        }
+    )
+}
 
 
+/*
 
 let show=()=>{
   removeTable();
@@ -29,6 +58,7 @@ let show=()=>{
   )
   ); 
 }
+*/
 
 let removeTable =()=>{var rowCount = document.getElementById('tab1').rows.length; 
             while(--rowCount) document.getElementById('tab1').deleteRow(rowCount)}
@@ -56,25 +86,47 @@ let addStudent=()=>{
   let name=document.getElementById('Student').value;
   let email=document.getElementById('Email').value;
 
-  fetch(baseURL+'add?name='+name+'&email='+email).then((resp)=>{alert("Student Added")});
+  fetch(baseURL+'add?name='+name+'&email='+email).then((resp)=>{console.log("Student Added")});
  show();
-
-
 
 
 }
 
-let deleteStudent=()=>{
+
+let deleteStudent = (id) => { 
+    fetch(baseURL+'delete?id='+id)
+    .then((resp) => {
+        
+        console.log('Student Deleted')
+        show();
+    })
+};
+
+/*let deleteStudent=()=>{
   let id=document.getElementById('ID').value;
   let x = 
   fetch(baseURL+'delete?id='+id).then((resp)=>{alert("Student Deleted")})
 };
 
+*/
 
-let updateStudent=()=>{
+
+let updateStudent = (id) => {
+    let updateName = document.getElementById('Student').value;
+    let updateEmail = document.getElementById('Email').value;
+    fetch(baseURL+'update?id='+id+'&name='+updateName+'&email='+updateEmail)
+    .then((resp) => {
+        console.log('Student Deleted')
+        show();
+    });
+}
+
+
+/*let updateStudent=()=>{
   let name=document.getElementById('name').value;
   let email=document.getElementById('email').value;
   let id=document.getElementById('id').value;
 
   fetch(baseURL+'update?id='+id+'&name='+name+'&email='+email).then((resp)=>{alert("Student Updated")});
 }
+*/
